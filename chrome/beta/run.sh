7a2c21c74915498eb1db12ef92a0581a2ad6da4a
Marco Matos <github@corp.mmatos.com>
2019-07-04 14:13:22 +0000

    updating with jessfraz dockerfiles

---
+++ chrome/beta/run.sh
@@ -0,0 +1,1 @@
+docker run -it --privileged --net host --cpuset-cpus 0 --memory 4gb -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY -v $HOME/Downloads:/home/chrome/Downloads -v $HOME/.config/google-chrome/:/data --device /dev/snd --device /dev/dri -v /dev/shm:/dev/shm --name chrome chrome
